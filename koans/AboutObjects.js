1df542776c38a47815cd9ce159bb4e89f9764ce7
jimtom2713 <jwilliamson528@gmail.com>
2015-05-08 23:19:43 -0400

    'updated AboutObjects.js. Tests still pass'

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -82,7 +82,7 @@ describe("About Objects", function () {
     expect("secretary" in meglomaniac).toBe(true);
     
     delete meglomaniac.henchman;
-    expect("henchman" in meglomaniac).toBe(false;
+    expect("henchman" in meglomaniac).toBe(false);
   });
 
 
